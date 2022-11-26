fa5cb277da6008d30115164e5134140784d3b308
shaman771 <shaman771@gmail.com>
2022-11-26 15:01:54 +0200

    add register user route

--- QA/boilerplate-advancednode/server.js
+++ QA/boilerplate-advancednode/server.js
@@ -1,5 +1,6 @@
 'use strict';
 require('dotenv').config();
+
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
@@ -39,8 +40,67 @@ myDB(async client => {
     res.render('index', {
       title: 'Connected to Database',
       message: 'Please log in',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
+  });
+
+  app
+    .route('/login')
+    .post(
+      passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res, next) => {
+        res.redirect('/profile');
+      });
+
+  app
+    .route('/profile')
+    .get(ensureAuthenticated, (req, res) => {
+      res.render('profile', { username: req.user.username });
+    });
+
+  app.route('/register')
+    .post((req, res, next) => {
+        myDataBase.findOne({ username: req.body.username }, (err, user) => {
+          if (err) {
+            next(err);
+          } else if (user) {
+            res.redirect('/');
+          } else {
+            myDataBase.insertOne({
+                username: req.body.username,
+                password: req.body.password
+              },
+              (err, doc) => {
+                if (err) {
+                  res.redirect('/');
+                } else {
+                  // The inserted document is held within
+                  // the ops property of the doc
+                  next(null, doc.ops[0]);
+                }
+              }
+            )
+          }
+        })
+      },
+      passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res, next) => {
+        res.redirect('/profile');
+      }
+    );
+
+  app.route('/logout')
+    .get((req, res) => {
+      req.logout({}, () => {
+        res.redirect('/');
+      });
+    });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
 
   passport.use('local', new LocalStrategy((username, password, done) => {
@@ -62,27 +122,6 @@ myDB(async client => {
       done(null, doc);
     });
   });
-
-  app
-    .route('/login')
-    .post(
-      passport.authenticate('local', { failureRedirect: '/' }),
-      (req, res, next) => {
-        res.redirect('profile');
-      });
-
-  app
-    .route('/profile')
-    .get(ensureAuthenticated, (req, res) => {
-      res.render('profile', { username: req.user.username });
-    });
-
-  app.route('/logout')
-    .get((req, res) => {
-      req.logout({}, () => {
-        res.redirect('/');
-      });
-    });
 
 }).catch(e => {
   app
